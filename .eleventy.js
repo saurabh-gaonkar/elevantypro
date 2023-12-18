f9affea1e6c13176c419447dd44e554906b51b06
saurabh-gaonkar <saurabhg@axioned.com>
2023-12-18 16:28:11 +0530

    upadated: location logic

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,27 @@
-// require("nunjucks");
-
-module.exports = function(eleventyconfig) {
-
-    eleventyconfig.addPassthroughCopy("./src/css/");
-    eleventyconfig.addWatchTarget("./src/css/");
+module.exports = function (eleventyConfig) {
+    // Passthrough copy for static files
+    eleventyConfig.addPassthroughCopy('src/static');
+    eleventyConfig.addPassthroughCopy('./src/css/');
+    eleventyConfig.addWatchTarget('./src/css/');
+  
+    // Add Nunjucks asynchronous shortcode for fetching location data
+    eleventyConfig.addNunjucksAsyncShortcode('getLocationData', async function () {
+      try {
+        const response = await fetch('https://your-netlify-app/.netlify/functions/getLocation');
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error fetching location:', error);
+        return { error: 'Internal Server Error' };
+      }
+    });
+  
+    // Other configurations...
     return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    }
+      dir: {
+        input: 'src',
+        output: 'public'
+      }
+    };
   };
+  
